f0465702bc117ce2ad6292dba9344d92b0f830e8
dlsso <supertonesfan@hotmail.com>
2014-06-26 10:29:50 -0600

    More events

--- main.js
+++ main.js
@@ -27,6 +27,14 @@ $(document).on('ready', function() {
   $('a').on('click', function(e){
   	console.log('clicked anchor')
 
+  	// "this" refers to the DOM element that was clicked
+  	console.log(this)
+
+  	//We can convert that DOM element into a jQuery object
+  	// by passing it to the $()
+  	$(this).css('color', 'red')
+  	$(this).text('clicked')
+
   	//Event-way to prevent the default behavior
   	// like navigating to a page when clicking an <a> tag
   	e.preventDefault()
